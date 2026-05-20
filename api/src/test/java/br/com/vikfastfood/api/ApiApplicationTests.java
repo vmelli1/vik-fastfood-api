fd7935cead184e5adae61d71afb04a265dfe7242
Victor Augusto de Carvalho Melli <vmelli595@gmail.com>
2026-05-20 15:55:10 -0300

    tentando corrigir bug do test

--- api/src/test/java/br/com/vikfastfood/api/ApiApplicationTests.java
+++ api/src/test/java/br/com/vikfastfood/api/ApiApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 @ActiveProfiles("test")
 class ApiApplicationTests {
 
